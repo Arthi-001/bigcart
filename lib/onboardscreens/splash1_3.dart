4a90bf68f3c463085b137fe46cd45e8c9df35439
Arthi_001 <arthi004j@gmail.com>
2026-03-14 12:13:52 +0530

    finished screens

---
+++ lib/onboardscreens/splash1_3.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class Splash13 extends StatefulWidget {
+  const Splash13({super.key});
+
+  @override
+  State<Splash13> createState() => _Splash13State();
+}
+
+class _Splash13State extends State<Splash13> {
+  @override
+  Widget build(BuildContext context) {
+    final Size size= MediaQuery.of(context).size;
+    return Stack(
+      children:[Image.asset( "assets/basket.jpg",width: double.infinity,
+      height: double.infinity,
+      fit: BoxFit.cover,
+      cacheWidth: 1080,), Scaffold(backgroundColor: Colors.transparent,
+      body:Column(
+        children: [
+          SizedBox(height: size.height*0.1,),
+          Center(
+            child: Text("Buy Premium",
+            style: TextStyle(
+              fontSize: 30,
+              fontWeight: FontWeight.w500),),
+          ),
+          Center(
+            child: Text(" Quality Fruits",
+            style: TextStyle(
+              fontSize: 30,
+              fontWeight: FontWeight.w500),),
+          ),
+          SizedBox(height: size.height*0.05,),
+          Center(child: Text("Find the best offers on top brands.",style: TextStyle( fontSize: 17,),)),
+         
+          
+      
+            ],) ,
+      ),
+    ]);
+  }
+}
