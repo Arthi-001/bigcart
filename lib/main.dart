f6c8a1510f3eeb978508b0baa8408b067e9d5529
Arthi_001 <arthi004j@gmail.com>
2026-03-14 12:19:45 +0530

    updated changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:bigcart/splash/splash.dart';
+import 'package:bigcart/onboardscreens/onboarding.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Splash()
+      home: Onboarding()
     );
   }
 }
